7c7577edd64a926fad1bf7a21eac2cdc3a5397d9
Dr00pyd00 <kottakcode@gmail.com>
2026-08-15 18:07:46 +0400

    feat(redis) hashs

--- Redis/main.py
+++ Redis/main.py
@@ -29,3 +29,33 @@ r.lpush('l1:emails', 'truc 3')
 
 l = r.lrange('l1:emails', 0, -1)
 print(l)
+
+# ====== dict = hash
+    # mapping pour pleins de champs en meme temps
+r.hset('dict1', mapping={
+    'date':'3232',
+    'heure':'4h00',
+    'statut':'dispo',
+    }
+       )
+
+dispo = r.hgetall('dict1')
+print(dispo)
+    
+    # avec un champ a la fois
+r.hset('d2', 'name', 'koko')
+r.hset('d2', 'age', '56')
+
+data = r.hgetall('d2')
+print(data)
+
+
+# le expire pour les hash se met APRES 
+r.expire('d2', 1)
+
+sleep(2)
+print(r.hgetall('d2'))
+
+
+
+
